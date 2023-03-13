98964916005eccbc15cd44e73e2ed33d21427c82
chenyao <yaochen_ws@163.com>
2023-03-13 23:14:13 +0800

    does not need this snippet yet

--- src/main/java/com/botwhy/chatgptland/config/WebMvcConfig.java
+++ src/main/java/com/botwhy/chatgptland/config/WebMvcConfig.java
@@ -1,24 +1,16 @@
 package com.botwhy.chatgptland.config;
 
-import com.botwhy.chatgptland.common.JacksonObjectMapper;
 
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-import java.util.List;
 
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
-    /**
-     * 设置静态资源映射
-     * @param registry
-     */
+
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
 //        System.out.println("注册了");
@@ -27,22 +19,5 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/*.html").addResourceLocations("classpath:/");
     }
 
-    /**
-     * 扩展mvc框架的消息转换器
-     * @param converters
-     */
-    @Override
-    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 
-        //创建消息转换器对象
-        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
-        //
-        JacksonObjectMapper mapper = new JacksonObjectMapper();
-        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS,false);
-        //设置对象转换器，底层使用Jackson将Java对象转为json
-
-        messageConverter.setObjectMapper(mapper);
-        //将上面的消息转换器对象追加到mvc框架的转换器集合中
-        converters.add(0,messageConverter);
-    }
 }
